5c22f7cda3ce862281c05627a68a5928eba0d6dd
Hwayoung Jung <hyj.main@gmail.com>
2025-03-13 23:11:48 +0900

    Added some documentation in between codes

--- project/01_Image_cluster_device_signal/outlier_detection.py
+++ project/01_Image_cluster_device_signal/outlier_detection.py
@@ -6,22 +6,27 @@
 ## visulaize with PCA
 
 def outlier_detection():
+    # load data
     path = 
     raw_df = 
     
-    
+    # standardize before outlier detection
     scaler = StandardScaler()
     scaler.fit(raw_df)
     df_scaled = scaler.transform(raw_df)
     
-    # DBscan
+    # Outlier detection with DBscan start
+    # calculate minimum points
     min_pts = raw_df.shape[1]*2
+    
     
     neighbors = NearestNeighbors(n_neighbors = min_pts, metric = 'euclidean')
     neighbors_fit = neighbors.fit(df_scaled)
     distances, indices = neighbors_fit.kneighbors(df_scaled)
     
     # eps = 1.5
+    
+    # Define distances
     distances = np.sort(distances, axis = 0)
     distances - distances[:,1]
     
@@ -30,7 +35,8 @@ def outlier_detection():
     # change x axis range to 3000 - 3500
     plt.xlim([75000,80000])
     plt.ylim([0,5])
-     
+    
+    # Decide epsilon to 1.5 and minimum samples with min_pts and run DBSCAN with the two parameters 
     m = DBSCAN(eps=1.5, min_samples=min_pts)
     m.fit(df_scaled)
 
